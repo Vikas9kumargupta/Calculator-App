1c6459314ca97f1adfa6cf5756200110f1d803d6
Vikas9kumargupta <vikas21xxx018@akgec.ac.in>
2023-02-09 20:04:36 +0530

    Basic Calculator App

---
+++ src/main/java/com/example/calculator/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
